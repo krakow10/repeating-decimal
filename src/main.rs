7ee41cd6c406c88529bcaeb107960af871df3f9b
Rhys Lloyd <krakow20@gmail.com>
2025-10-06 15:55:28 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+pub fn modpow(mut base: u64, mut exp: u64, n: u64) -> u64 {
+    if exp == 0 {
+        return 1;
+    }
+
+    let mut res = 1;
+    base %= n;
+
+    loop {
+        if exp & 1 == 1 {
+            res *= base;
+            res %= n;
+        }
+
+        if exp == 1 {
+            return res;
+        }
+
+        exp >>= 1;
+        base *= base;
+        base %= n;
+    }
+}
+
+fn main() {
+	for i in 1..10000{
+		for p in 1..i{
+			if modpow(10,p,i)==1{
+				println!("1/{i} has {p} repeating digits");
+				break;
+			}
+		}
+	}
+}
